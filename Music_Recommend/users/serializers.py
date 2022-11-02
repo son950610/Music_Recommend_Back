26f9b66ebf1a9a3be1e098cde41a060876ed1273
ssa <wogur981208@gmail.com>
2022-11-03 00:01:17 +0900

    Feat : 비밀번호 변경API, user모델에 프로필 이미지 수정

--- Music_Recommend/users/serializers.py
+++ Music_Recommend/users/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import check_password
+
 from .models import User
 from .validators import contains_special_character, contains_uppercase_letter, contains_lowercase_letter, contains_number
 
@@ -7,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ('email', 'nickname', 'password', 'repassword',)
+        fields = ('email', 'nickname', 'password', 'repassword','profile_image',)
         extra_kwargs = {'email': {
                         'error_messages': {
                         'required': '이메일을 입력해주세요.',
@@ -33,17 +35,18 @@ class UserSerializer(serializers.ModelSerializer):
         if contains_special_character(nickname) or len(nickname) < 3:
             raise serializers.ValidationError(detail={"nickname":"닉네임은 2자 이하 또는 특수문자를 포함할 수 없습니다."})
         
-        if password != repassword:
-            raise serializers.ValidationError(detail={"password":"비밀번호가 일치하지 않습니다."})
-        
-        if ( len(password) < 8 or len(password) > 17 
-            or not contains_uppercase_letter(password)
-            or not contains_lowercase_letter(password)
-            or not contains_number(password) 
-            or not contains_special_character(password) 
-            ):
-            raise serializers.ValidationError(detail={"password":"비밀번호는 8자 이상 16자이하의 영문 대/소문자, 숫자, 특수문자 조합이어야 합니다. "})
-        
+        if password:
+            if password != repassword:
+                raise serializers.ValidationError(detail={"password":"비밀번호가 일치하지 않습니다."})
+            
+            if ( len(password) < 8 or len(password) > 17 
+                or not contains_uppercase_letter(password)
+                or not contains_lowercase_letter(password)
+                or not contains_number(password) 
+                or not contains_special_character(password) 
+                ):
+                raise serializers.ValidationError(detail={"password":"비밀번호는 8자 이상 16자이하의 영문 대/소문자, 숫자, 특수문자 조합이어야 합니다. "})
+            
         return data
     
     def create(self, validated_data):
@@ -56,13 +59,47 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        instance.nickname = validated_data.get('nickname', instance.nickname)
+        instance.profile_image = validated_data.get('profile_image', instance.profile_image)
+
+        instance.save()
+        return instance
+
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    repassword= serializers.CharField(error_messages={'required':'비밀번호를 입력해주세요.', 'blank':'비밀번호를 입력해주세요.'})    
+    class Meta:
+        model = User
+        fields = ('password', 'repassword',)
+        extra_kwargs = {'password':{'write_only':True,
+                        'error_messages': {
+                        'required':'비밀번호를 입력해주세요.',
+                        'blank':'비밀번호를 입력해주세요.',}},}
+
+    def validate(self, data):
+        current_password = self.context.get("request").user.password
+        password = data.get('password')
+        repassword = data.get('repassword')
+        
+        if check_password(password, current_password):
+            raise serializers.ValidationError(detail={"password":"현재 사용중인 비밀번호와 동일한 비밀번호는 입력할 수 없습니다."})
+        
+        if password != repassword:
+            raise serializers.ValidationError(detail={"password":"비밀번호가 일치하지 않습니다."})
+            
+        if ( len(password) < 8 or len(password) > 17 
+                or not contains_uppercase_letter(password)
+                or not contains_lowercase_letter(password)
+                or not contains_number(password) 
+                or not contains_special_character(password) 
+                ):
+                raise serializers.ValidationError(detail={"password":"비밀번호는 8자 이상 16자이하의 영문 대/소문자, 숫자, 특수문자 조합이어야 합니다. "})
+            
+        return data
     
-    def update(self, validated_data):
-        email = validated_data['email']
-        nickname = validated_data['nickname']
-        user = User(email=email, nickname=nickname)
-        user.save()
-        return user
-
-
-
+    def update(self, instance, validated_data):
+        instance.password = validated_data.get('password', instance.password)
+        instance.set_password(instance.password)
+        instance.save()
+        return instance
--- Music_Recommend/users/views.py
+++ Music_Recommend/users/views.py
@@ -3,9 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.generics import get_object_or_404
+from django.contrib.auth.hashers import check_password
 from .jwt_claim_serializer import CustomTokenObtainPairSerializer
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, ChangePasswordSerializer
 from .models import User
 
 # Create your views here.
@@ -27,7 +28,7 @@ class UserView(APIView):
             if serializer.is_valid():
                 serializer.save()
                 return Response({"message":"회원정보 수정 성공"} , status=status.HTTP_201_CREATED)
-            return Response({"message":"회원정보 수정 실패"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response({"message":"접근 권한 없음"}, status=status.HTTP_403_FORBIDDEN)
 
     #회원탈퇴
@@ -37,6 +38,28 @@ class UserView(APIView):
             user.delete()
             return Response({"message":"회원탈퇴 성공"}, status=status.HTTP_204_NO_CONTENT)
         return Response({"message":"회원탈퇴 실패"}, status=status.HTTP_400_BAD_REQUEST)
+
+class ChangePasswordView(APIView):
+    #비밀번호 인증
+    def post(self, request):
+        user = get_object_or_404(User, id=request.user.id)
+        password = user.password
+        if check_password(request.data["password"], password):
+            return Response({"message":"인증이 완료되었습니다."}, status=status.HTTP_200_OK)        
+        else:
+            return Response({"message":"맞는 비밀번호를 적어주세요."}, status=status.HTTP_400_BAD_REQUEST)
+
+            
+    #비밀번호 변경
+    def put(self, request):
+        user = get_object_or_404(User, id=request.user.id)
+        if user == request.user:
+            serializer = ChangePasswordSerializer(user, data=request.data, context={'request': request})
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"message":"비밀번호 변경이 완료되었습니다! 다시 로그인해주세요."} , status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message":"접근 권한 없음"}, status=status.HTTP_403_FORBIDDEN)
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
