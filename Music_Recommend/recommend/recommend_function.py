f3ae012835939fd866bb7293c3b1bb8f33f576a8
Jay <jvidorca@gmail.com>
2022-11-03 23:34:00 +0900

    Feat : import modules to recommend_function.py
    -1. recommend/recommend_function.py
    -2. import modules and import data from DB

--- Music_Recommend/recommend/recommend_function.py
+++ Music_Recommend/recommend/recommend_function.py
@@ -6,3 +6,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Music_Recommend.settings')
 django.setup()
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
+from songs.models import Song
+
+# DB에서 데이터 가져오기
+data = pd.DataFrame(list(Song.objects.values()))
