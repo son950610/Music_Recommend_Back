5b1c2889fc574a107eb065862540bbdfea473cdc
Jay <jvidorca@gmail.com>
2022-11-03 23:56:28 +0900

    Feat : modify urls.py
    -1. recommend/urls.py
    -2. fix urls from songs/<int:song_id>/ to songs/recommend/<int:song_id>/

--- Music_Recommend/recommend/urls.py
+++ Music_Recommend/recommend/urls.py
@@ -3,5 +3,5 @@ from recommend import views
 
 urlpatterns = [
     path('songs/', views.SongListView.as_view(), name="song-list"),    
-    path('songs/<int:song_id>/', views.SongRecommendView.as_view(), name="song-recommend"),   
+    path('songs/recommend/<int:song_id>/', views.SongRecommendView.as_view(), name="song-recommend"),   
 ]
