8cae76bb03125607d38841cbbaec9c171be3cff8
손상훈 <son950610@naver.com>
2022-11-04 00:51:19 +0900

    Feat: Comment 모델 생성
    - Comment 모델 생성

--- Music_Recommend/songs/models.py
+++ Music_Recommend/songs/models.py
@@ -35,4 +35,12 @@ class Voice(models.Model):
         db_table = 'voice'
         ordering = ['-created_at']
 
-
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE, related_name="comments")
+    content= models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
+    
+    def __str__(self):
+        return str(self.content)
