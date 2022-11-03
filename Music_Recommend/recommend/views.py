8d7c37e1b40b4603ae6aaf1e5a3c19890c188ce1
Jay <jvidorca@gmail.com>
2022-11-03 23:40:30 +0900

    Fix : fix typo in views.py
    -1. recommend/views.py
    -2. fix typo in module name

--- Music_Recommend/recommend/views.py
+++ Music_Recommend/recommend/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_list_or_404
 from rest_framework import status 
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from recommend.recom import get_recommendations
+from recommend.recommend_function import get_recommendations
 import random
 
 from songs.models import Song
