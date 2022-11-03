0f500de66e382d3deee20be2e40f4c1d783c3577
손상훈 <son950610@naver.com>
2022-11-04 00:54:23 +0900

    Feat: Comment url 추가
    - Comment url 추가 (목록,상세url)

--- Music_Recommend/songs/urls.py
+++ Music_Recommend/songs/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('<int:song_id>/', views.SongView.as_view(), name='song_view'),
     path('<int:song_id>/song_like/', views.SongLikeView.as_view(), name='song_like_view'),
+    path('<int:song_id>/comment/', views.CommentView.as_view(), name='comment_view'),
+    path('<int:song_id>/comment/<int:comment_id>/', views.CommentDetailView.as_view(), name='comment_detail_view'),
 ]
