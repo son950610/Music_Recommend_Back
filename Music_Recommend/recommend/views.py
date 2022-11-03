4fa8b29acab5e1d1d794b55bff12e88ed233bde3
Jay <jvidorca@gmail.com>
2022-11-03 23:28:03 +0900

    Feat : add View(SongListView)
    -1. recommend/views.py
    -2. add SongListView

--- Music_Recommend/recommend/views.py
+++ Music_Recommend/recommend/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404
+from rest_framework import status 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from recommend.recom import get_recommendations
+import random
 
-# Create your views here.
+from songs.models import Song
+from recommend.serializers import (
+    SongListSerializer,
+    )
+
+
+class SongListView(APIView):
+    def get(self, request, format=None):
+        song_list = get_list_or_404(Song)
+        listserializer = SongListSerializer([random.choice(song_list) for i in range(12)], many=True)
+        return Response(listserializer.data, status=status.HTTP_200_OK)
