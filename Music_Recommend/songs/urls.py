6f32e57d2cfc8891502d68070a9bb93853f3aace
jiyeong <2jiyeong0@gmail.com>
2022-11-03 15:08:50 +0900

    Feat : 노래 상세 조회 기능 추가
    -1. SongView 노래 상세조회 기능 추가

---
+++ Music_Recommend/songs/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+from . import views
+
+urlpatterns = [
+    path('<int:song_id>/', views.SongView.as_view(), name='song_view'),
+]
--- Music_Recommend/songs/views.py
+++ Music_Recommend/songs/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
+from rest_framework import status, permissions
+from rest_framework.response import Response
 from .models import Song
+from songs.serializers import SongSerializer
 # Create your views here.
 
 
+class SongView(APIView):
+    def get(self, request, song_id):
+        song = get_object_or_404(Song, id=song_id)
+        serializer = SongSerializer(song)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
 class SearchView(APIView):
     def get(self, request):
         post_result = Song.object.all()
