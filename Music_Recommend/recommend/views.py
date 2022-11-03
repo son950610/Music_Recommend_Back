4fe8c284767bd392ddeff5104ba47448c2bb6a0f
Jay <jvidorca@gmail.com>
2022-11-03 23:30:16 +0900

    Feat : add SongRecommendView
    -1. recommend/views.py
    -2. add SongRecommendView

--- Music_Recommend/recommend/views.py
+++ Music_Recommend/recommend/views.py
@@ -16,3 +16,12 @@ class SongListView(APIView):
         song_list = get_list_or_404(Song)
         listserializer = SongListSerializer([random.choice(song_list) for i in range(12)], many=True)
         return Response(listserializer.data, status=status.HTTP_200_OK)
+
+
+class SongRecommendView(APIView):
+    def post(self, request, song_id, format=None):
+        slt_song = get_list_or_404(Song, id=song_id)
+        print(slt_song[0].pk)
+        rcm_list = get_recommendations(slt_song[0].pk)
+        print(rcm_list)
+        return Response(rcm_list, status=status.HTTP_200_OK)
