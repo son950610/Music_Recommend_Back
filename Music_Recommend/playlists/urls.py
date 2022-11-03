b38f538483da05b93d44dadf77d56f234c008d69
ssa <wogur981208@gmail.com>
2022-11-03 19:21:20 +0900

    Feat : PlaylistView, PlaylistDetailView 기능 구현

--- Music_Recommend/playlists/urls.py
+++ Music_Recommend/playlists/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:user_id>/<int:playlist_id>/', views.PlaylistView.as_view(), name='playlist_view'),
-    path('<int:user_id>/<int:playlist_id>/<int:song_id>/', views.PlaylistDetailView.as_view(), name='playlist_detail_view'),
+    path('', views.PlaylistView.as_view(), name='playlist_view'),
+    path('<int:playlist_id>/', views.PlaylistDetailView.as_view(), name='playlist_detail_view'),
 ]
--- Music_Recommend/playlists/views.py
+++ Music_Recommend/playlists/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from .models import Playlist
+from .serializers import PlaylistCreateSerializer, PlaylistSerializer, PlaylistDetailSerializer
 # Create your views here.
+class PlaylistView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        playlist = Playlist.objects.filter(user=request.user.id)
+        serializer = PlaylistSerializer(playlist, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+            
+    def post(self, request):
+        serializer = PlaylistCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PlaylistDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, playlist_id):
+        playlist = get_object_or_404(Playlist, id=playlist_id )
+        serializer = PlaylistDetailSerializer(playlist)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
+    def put(self, request, playlist_id):
+        playlist = get_object_or_404(Playlist, id=playlist_id )
+        if request.user == playlist.user:
+            serializer = PlaylistCreateSerializer(playlist, data=request.data)
+            if serializer.is_valid():
+                serializer.save(user=request.user)
+                return Response(serializer.data , status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message":"접근 권한 없음"}, status=status.HTTP_403_FORBIDDEN)
+    
+    def delete(self, request, playlist_id):
+        playlist = get_object_or_404(Playlist, id=playlist_id )
+        if request.user == playlist.user:
+            playlist.delete()
+            return Response({"message":"삭제 완료"}, status=status.HTTP_204_NO_CONTENT)
+        return Response({"message":"접근 권한 없음"}, status=status.HTTP_403_FORBIDDEN)
