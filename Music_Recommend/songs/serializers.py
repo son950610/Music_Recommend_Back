85b2a4d8875fba147659982e174bc6d692f7e5fc
손상훈 <son950610@naver.com>
2022-11-04 01:02:27 +0900

    Feat: Comment CRUD 기능 추가
    - Comment views.py에 CRUD 추가
    - serializers.py에 CommentSerializer, CommentCreateSerializer
    - songs admin.py에 Comment 추가

--- Music_Recommend/songs/serializers.py
+++ Music_Recommend/songs/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
-from songs.models import Song
+from songs.models import Song, Comment
 
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = "__all__"
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    class Meta:
+        model = Comment
+        fields = "__all__"
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('content',)
--- Music_Recommend/songs/views.py
+++ Music_Recommend/songs/views.py
@@ -3,8 +3,8 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from .models import Song
-from songs.serializers import SongSerializer
+from .models import Song, Comment
+from songs.serializers import SongSerializer, CommentSerializer, CommentCreateSerializer
 # Create your views here.
 
 
@@ -29,3 +29,43 @@ class SearchView(APIView):
     def get(self, request):
         post_result = Song.object.all()
     pass
+
+
+class CommentView(APIView):
+    
+    def get(self, request, song_id):
+        song = Song.objects.get(id=song_id)
+        comments = song.comment_set.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, song_id):
+        serializer = CommentCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user, article_id=song_id)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetailView(APIView):
+
+    def put(self, request, song_id, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        if request.user == comment.user:
+            serializer = CommentCreateSerializer(comment, data=request.data)
+            if serializer.is_vaild():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response("권한이 없습니다!", status=status.HTTP_403_FORBIDDEN)
+
+    def delete(self, request, comment_id):
+        comment= get_object_or_404(Comment, id=comment_id)
+        if request.user == comment.user:
+            comment.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response("권한이 없습니다!", status=status.HTTP_403_FORBIDDEN)
