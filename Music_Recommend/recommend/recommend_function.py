83e4904b5d6afc16eab6476bf100cbedea417f4f
Jay <jvidorca@gmail.com>
2022-11-03 23:31:48 +0900

    Feat : add recommend_function.py
    -1. recommend/recommend_function.py
    -2. create file and add settings

---
+++ Music_Recommend/recommend/recommend_function.py
@@ -0,0 +1,8 @@
+import sys
+import os
+import django
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Music_Recommend.settings')
+django.setup()
