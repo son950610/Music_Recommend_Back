2158511483f6772249466f825151f0f1d8039efe
ssa <wogur981208@gmail.com>
2022-11-06 13:39:30 +0900

    Feat : 프로필 serializer 추가

--- Music_Recommend/songs/views.py
+++ Music_Recommend/songs/views.py
@@ -46,12 +46,13 @@ class SongLikeView(APIView):
 #노래 검색
 class SearchView(PaginationHandlerMixin, APIView):
     pagination_class = SearchPagination
-    permission_classes = [IsAuthenticated] 
+    # permission_classes = [IsAuthenticated] 
     
     def get(self, request):
         keyword = request.GET.get('keyword')
         if keyword =='':
             return Response("검색 결과가 없습니다.", status=status.HTTP_200_OK)
+        
         post_result = Song.objects.filter(
         Q(title__icontains=keyword) |
         Q(singer__icontains=keyword)|
@@ -59,6 +60,7 @@ class SearchView(PaginationHandlerMixin, APIView):
         )
         page = self.paginate_queryset(post_result)
         serializer = self.get_paginated_response(SearchSerializer(page,many=True).data)
+    
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
